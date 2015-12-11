d679ba4654c6fc3f943e160bc5a3de511aca9f67
Jacob Shay <Jacob Shay>
2015-12-10 23:03:25 -0500

    first commit

---
+++ RandomEarnings/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomEarnings
+{
+    public class Class1
+    {
+        double salary  { get; set; }
+
+        public double getSalary()
+        {
+            Random r = new Random();
+            salary = r.Next(10000, 200000);
+            return salary;
+        }
+
+    }
+}
